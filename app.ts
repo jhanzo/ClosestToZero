39805df63491dd3a395ce0069b918154af6a1147
unknown <sam.capela@hotmail.fr>
2020-05-29 15:10:33 +0200

    commit

--- app.ts
+++ app.ts
@@ -15,5 +15,10 @@ export const minDistance = (inputs: number[] = []): number => {
         const newMin = sorted[i + 1] - sorted[i];
         min = newMin < min && newMin > 0 ? newMin : min;
     }
+
+    if(min < 0)
+    {
+        return 0;
+    }
     return min;
 };
