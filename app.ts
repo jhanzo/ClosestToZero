cd3771cd0cd827676f40214a62f524d77918b408
unknown <sam.capela@hotmail.fr>
2020-05-29 14:51:13 +0200

    app

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
 export const closestToZero = (inputs: number[] = []): number => {
     if (!inputs || inputs.length == 0) {
-        return 0
+        return 0;
     }
 
     const sorted_list = inputs.map(e => Math.abs(e)).sort((a, b) => a < b ? -1 : 1)
-    const first = sorted_list[0]
-    return inputs.indexOf(first) !== -1 ? first : -first
+    const first = sorted_list[0];
+    return inputs.indexOf(first) !== -1 ? first : -first;
 };
 
 export const minDistance = (inputs: number[] = []): number => {
